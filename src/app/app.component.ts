66f27d06c9f021c24df0228a512f3e5447e49827
kyrylo gerasymenko <kyrylo.gerasymenko@globallogic.com>
2017-11-02 14:35:48 +0200

    very naive first steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  onTimerFinish() {
+    console.log('time is out');
+  }
 }
---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+
+  @Input() interval;
+  @Output() finish = new EventEmitter();
+
+  timeLeft = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.timeLeft = this.interval;
+    const handle = setInterval(() => {
+      this.timeLeft--;
+      if (this.timeLeft <= 0) {
+        clearInterval(handle);
+        this.finish.emit();
+      }
+    }, 1000);
+  }
+
+}
